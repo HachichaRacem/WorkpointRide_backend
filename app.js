916ce0bfd14fed523b487a47397e7184246ef4ff
workpoint.team <workpoint.team@gmail.com>
2024-05-22 10:05:12 +0100

    conf

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use("/api/reservations", reservationRouter);
 app.use("/api/schedules", scheduleRouter);
 app.use("/api/vehicles", vehicleRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5030;
 
 mongoose
   .connect(process.env.DB_KEY)
